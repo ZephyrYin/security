fe8e3c0f4b32f168a88c3a06b4380f975dc6af66
Marco <phantomlyc@gmail.com>
2015-11-02 23:50:59 -0500

    Add Prompt upon install

---
+++ example/background.js
@@ -0,0 +1,19 @@
+
+function popupwindow(url, title, w, h) {
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);
+  return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+
+
+// Check whether new version is installed
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+		windowName = "Information Collection";
+		popupwindow("getInfo.html", windowName, screen.width / 1.5, screen.height / 1.5);
+    }
+});
