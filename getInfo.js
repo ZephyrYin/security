0369e3482664854237018b5aec968fc94330a030
Marcolyc <phantomlyc@gmail.com>
2015-11-12 20:44:39 -0500

    Use chrome local storage to save info

--- getInfo.js
+++ getInfo.js
@@ -9,15 +9,23 @@ document.addEventListener('DOMContentLoaded',
 					alert("something wrong!")
 				}
 				
-				localStorage.setItem('FirstName', form[0].value);
-				localStorage.setItem('LastName', form[1].value);
-				localStorage.setItem('DateOfBirth', form[2].value);
-				localStorage.setItem('Email', form[3].value);				
-				localStorage.setItem('Address', form[4].value);
-				localStorage.setItem('PhoneNumber', form[5].value);
+				chrome.storage.local.set( {'FirstName' : form[0].value});
+				chrome.storage.local.set( {'LastName' : form[1].value});
+				chrome.storage.local.set( {'DateOfBirth' : form[2].value});
+				chrome.storage.local.set( {'Email' : form[3].value});			
+				chrome.storage.local.set( {'Address' : form[4].value});
+				chrome.storage.local.set( {'PhoneNumber' : form[5].value});
+				
+				/* Test If we can get information 
+				chrome.storage.local.get(['FirstName', 'LastName'], function(ret) {
+					console.log("FirstName Saved is: " + ret.FirstName);
+					console.log("LastName Saved is: " + ret.LastName);
+				});
+				*/
 				
 				alert("Saving Information to Local Storage");
 				window.close();
+			
 			}
 		);
 		
